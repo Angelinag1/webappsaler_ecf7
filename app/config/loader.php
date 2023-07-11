b14b8eb4011a6d0a200dbbd9bb53c29e0649dbc3
angydev987 <angelina.garaebiti2000@gmail.com>
2023-07-11 16:58:26 +1100

    initi

---
+++ app/config/loader.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phalcon\Loader;
+
+$loader = new Loader();
+
+/**
+ * Register Namespaces
+ */
+$loader->registerNamespaces([
+    'Webappsaler\Models' => APP_PATH . '/common/models/',
+    'Webappsaler'        => APP_PATH . '/common/library/',
+]);
+
+/**
+ * Register module classes
+ */
+$loader->registerClasses([
+    'Webappsaler\Modules\Frontend\Module' => APP_PATH . '/modules/frontend/Module.php',
+    'Webappsaler\Modules\Cli\Module'      => APP_PATH . '/modules/cli/Module.php'
+]);
+
+$loader->register();
